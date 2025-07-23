1647444ea8d5a9232affd37e6c1f8f7c6eda8f2b
Guillaume Philippe <157108514+LaGuibole@users.noreply.github.com>
2025-07-23 14:04:25 +0200

    + | Bot is now working properly, promoting a new operator if the operator was kick. Still, i need to retrieve David's work on nickname conflicts

--- srcs/Bot.cpp
+++ srcs/Bot.cpp
@@ -6,7 +6,7 @@
 /*   By: guphilip <guphilip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 23:08:46 by guillaumeph       #+#    #+#             */
-/*   Updated: 2025/07/23 13:48:40 by guphilip         ###   ########.fr       */
+/*   Updated: 2025/07/23 14:01:22 by guphilip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,64 @@ void Bot::loadWordlist()
 //     return true;
 // }
 
+// bool Bot::inspectMessages(Client* client, const std::string& message, Channel* channel, ChannelManager* channelManager)
+// {
+//     for (size_t i = 0; i < _wordlist.size(); ++i)
+//     {
+//         if (message.find(_wordlist[i]) != std::string::npos)
+//         {
+//             _warnings[client]++;
+//             int count = _warnings[client];
+//             std::string nick = client->getNickname();
+
+//             if (!channel)
+//                 return false;
+
+//             if (count > 2)
+//             {
+//                 client->reply(":BOT NOTICE " + nick + " :[BOT] You have been removed from " + channel->getName() + " for repeated inappropriate language.");
+
+//                 bool wasOperator = channel->isOperator(client);
+
+//                 std::string partMsg = ":" + client->getPrefix() + " PART " + channel->getName() + " :[BOT] Repeated inappropriate language.";
+//                 channel->broadcast(partMsg);
+
+//                 channel->removeMember(client, channelManager);
+
+//                 if (wasOperator && channel->getOperators().empty() && !channel->getMembers().empty())
+//                 {
+//                     Client* newOp = channel->getMembers().front();
+//                     channel->addOperator(newOp);
+
+//                     std::string joinMsg = ":localhost " + newOp->getPrefix() + " JOIN " + channel->getName();
+//                     newOp->reply(joinMsg);
+
+//                     channel->sendNamesListTo(newOp);
+
+//                     std::string modeMsg = ":localhost MODE " + channel->getName() + " +o " + newOp->getNickname();
+//                     channel->broadcast(modeMsg);
+
+//                     std::stringstream opMsg;
+//                     opMsg << ":BOT PRIVMSG " << channel->getName()
+//                           << " :[BOT] " << newOp->getNickname() << " has been promoted to channel operator.";
+//                     channel->broadcast(opMsg.str());
+//                 }
+//             }
+//             else
+//             {
+//                 std::stringstream warnMsg;
+//                 warnMsg << ":BOT PRIVMSG " << channel->getName()
+//                         << " :[BOT] Warning " << count << "/2 for " << nick << " : inappropriate language detected.";
+//                 channel->broadcast(warnMsg.str());
+//             }
+
+//             std::cout << "[BOT] Warning " << count << "/2 for " << nick << std::endl;
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
 bool Bot::inspectMessages(Client* client, const std::string& message, Channel* channel, ChannelManager* channelManager)
 {
     for (size_t i = 0; i < _wordlist.size(); ++i)
@@ -98,30 +156,35 @@ bool Bot::inspectMessages(Client* client, const std::string& message, Channel* c
                 client->reply(":BOT NOTICE " + nick + " :[BOT] You have been removed from " + channel->getName() + " for repeated inappropriate language.");
 
                 bool wasOperator = channel->isOperator(client);
+                bool shouldPromote = wasOperator && channel->getOperators().size() == 1 && channel->getMembers().size() > 1;
+
+                Client* newOp = NULL;
+                if (shouldPromote)
+                {
+                    std::vector<Client*> members = channel->getMembers();
+                    for (size_t j = 0; j < members.size(); ++j)
+                    {
+                        if (members[j] != client)
+                        {
+                            newOp = members[j];
+                            channel->addOperator(newOp);
+
+                            std::string modeMsg = ":localhost MODE " + channel->getName() + " +o " + newOp->getNickname();
+                            channel->broadcast(modeMsg);
+
+                            std::stringstream opMsg;
+                            opMsg << ":BOT PRIVMSG " << channel->getName()
+                                  << " :[BOT] " << newOp->getNickname() << " has been promoted to channel operator.";
+                            channel->broadcast(opMsg.str());
+                            break;
+                        }
+                    }
+                }
 
                 std::string partMsg = ":" + client->getPrefix() + " PART " + channel->getName() + " :[BOT] Repeated inappropriate language.";
                 channel->broadcast(partMsg);
 
                 channel->removeMember(client, channelManager);
-
-                if (wasOperator && channel->getOperators().empty() && !channel->getMembers().empty())
-                {
-                    Client* newOp = channel->getMembers().front();
-                    channel->addOperator(newOp);
-
-                    std::string joinMsg = ":localhost " + newOp->getPrefix() + " JOIN " + channel->getName();
-                    newOp->reply(joinMsg);
-
-                    channel->sendNamesListTo(newOp);
-
-                    std::string modeMsg = ":localhost MODE " + channel->getName() + " +o " + newOp->getNickname();
-                    channel->broadcast(modeMsg);
-
-                    std::stringstream opMsg;
-                    opMsg << ":BOT PRIVMSG " << channel->getName()
-                          << " :[BOT] " << newOp->getNickname() << " has been promoted to channel operator.";
-                    channel->broadcast(opMsg.str());
-                }
             }
             else
             {
